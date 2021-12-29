5e0f2dce703584ceb2d5749631b446038ebcd83d
schperplata <domen.jurkovic@gmail.com>
2021-12-29 14:56:05 +0100

    TIM Counter is set to 0 at power on/off

--- user/sun_ctrl.c
+++ user/sun_ctrl.c
@@ -10,16 +10,15 @@ extern configuration_t cfg_data;
 
 void sun_init(void)
 {
-  LL_TIM_EnableAllOutputs(SUN_TIM);
   LL_TIM_SetCounter(SUN_TIM, 0);
   sun_set_intensity(cfg_data.sun_manual_intensity);
+  LL_TIM_EnableAllOutputs(SUN_TIM);
 }
 
 void _sun_pwr_ctrl(bool state)
 {
   if (state)
   {
-    LL_TIM_SetCounter(SUN_TIM, 0);
     LL_TIM_CC_EnableChannel(SUN_TIM, SUN_TIM_CH);
     LL_TIM_EnableCounter(SUN_TIM);
   }
@@ -28,6 +27,7 @@ void _sun_pwr_ctrl(bool state)
     LL_TIM_CC_DisableChannel(SUN_TIM, SUN_TIM_CH);
     LL_TIM_DisableCounter(SUN_TIM);
   }
+  LL_TIM_SetCounter(SUN_TIM, 0);
 }
 
 void sun_pwr_on(void)
@@ -39,6 +39,7 @@ void sun_pwr_off(void)
 {
   _sun_pwr_ctrl(false);
 }
+
 void sun_pwr_on_manual(void)
 {
   sun_set_intensity(cfg_data.sun_manual_intensity);
@@ -61,6 +62,7 @@ void sun_set_intensity(uint8_t intensity)
 
   // percent to value conversion
   uint32_t compare_val = get_sun_intensity_resolution() / SUN_INTENSITY_MAX * intensity;
+
   LL_TIM_OC_SetCompareCH4(SUN_TIM, compare_val);
 }
 
