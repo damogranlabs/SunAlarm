5a594ffa38fe2efad79cd09bf3fb061d5ef79b9f
schperplata <domen.jurkovic@gmail.com>
2022-01-07 20:09:02 +0100

    Fixed default time.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,19 +2,16 @@
 /**
  ******************************************************************************
  * @file           : main.c
- * @brief          : Main program body
+ * @brief          : Main program body 
  ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- ******************************************************************************
+ * 
+ * SunAlarm
+ * @date    Jan-2022
+ * @author  Domen Jurkovic
+ * @source  http://damogranlabs.com/
+ *          https://github.com/damogranlabs/SunAlarm 
+ * 
+ * ******************************************************************************
  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
@@ -115,19 +112,14 @@ int main(void)
   lcd_clear();
   ctrl_lcd_backlight(true, true);
 
-  // TODO load cfg data from flash
   set_defaults();
   sun_init();
   show_alarm_state();
-
+  set_new_time(7, 0, 0);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  set_new_time(6, 28, 55);
-
-  // save_settings();
-  // read_settings();
   while (1)
   {
     if (rtc_event)
